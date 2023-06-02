2a8edf0929cc753c251803ef98f36e343eacc286
mkutay <mkutaybzkrt01101011@gmail.com>
2023-06-02 21:27:45 +0300

    test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require('fs');
 
 const app = express();
 
@@ -12,8 +13,26 @@ app.get("/", (req, res) => {
 
 app.post("/add", (req, res) => {
   const { a, b } = req.body;
+  let sum = parseInt(a) + parseInt(b);
+  let jsn = {"data": []};
+  console.log(sum);
+  fs.readFile('./data.json', 'utf8', (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    jsn = JSON.parse(data);
+  });
+  jsn["data"].push({"a": a, "b": b, "sum": sum});
+  fs.writeFile('./data.json', JSON.stringify(jsn), (error) => {
+    if (error) {
+      console.log("there has been an error");
+      return;
+    }
+    console.log("success");
+  })
   res.send({
-    result: parseInt(a) + parseInt(b)
+    result: sum,
   });
 });
 
